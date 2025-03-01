78f8be7274ccdbef4511374edb1ca903a0d235f8
Lozzbytech <anurag03690@gmail.com>
2025-03-01 23:15:40 +0530

    Add files via upload

---
+++ bootstrap/cache/livewire-components.php
@@ -0,0 +1,8 @@
+<?php return array (
+  'posts.create' => 'App\\Http\\Livewire\\Posts\\Create',
+  'posts.timeline' => 'App\\Http\\Livewire\\Posts\\Timeline',
+  'posts.view' => 'App\\Http\\Livewire\\Posts\\View',
+  'profile.profile-page' => 'App\\Http\\Livewire\\Profile\\ProfilePage',
+  'users.edit' => 'App\\Http\\Livewire\\Users\\Edit',
+  'users.index' => 'App\\Http\\Livewire\\Users\\Index',
+);
